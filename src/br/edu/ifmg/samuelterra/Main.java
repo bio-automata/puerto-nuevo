99d4b23d5447562964bc7da315bbef21f4b1aa65
Samuel Terra <samuelterra22@hotmail.com>
2017-05-02 19:20:20 -0300

    Evento parcialmente modelado. Sets definidos.

--- src/br/edu/ifmg/samuelterra/Main.java
+++ src/br/edu/ifmg/samuelterra/Main.java
@@ -1,21 +1,32 @@
 package br.edu.ifmg.samuelterra;
 
+import br.edu.ifmg.samuelterra.model.Carreta;
+import br.edu.ifmg.samuelterra.model.Grua;
 import br.edu.ifmg.samuelterra.model.navios.ContainerShip;
+
+import java.util.Queue;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ContainerShip containerShip = new ContainerShipFactory().cria(2);
+        /*ContainerShip containerShip = new ContainerShipFactory().cria(2);
         System.out.println(containerShip.getNome());
 
         ContainerShip containerShip2 = new ContainerShipFactory().cria(6);
+        System.out.println(containerShip2.getNome());*/
 
-        System.out.println(containerShip2.getNome());
+        Queue<ContainerShip> naviosChegada;
+        Queue<ContainerShip> naviosAguardandoDescarregar;
+        Queue<ContainerShip> naviosSaida;
+        Queue<Carreta> filaCarretaPorGrua;
+        Queue<Carreta> filaCarretaAguardandoDescarregarContainerPatio;
 
-        // simular chegadas de navios
-
-        // simular chegadas de caminhoes
+        Set<Grua> setGruas;
+        Set<Grua> setEquipes;
+        Set<Grua> setCais;
+        Set<Grua> setCarretas;
 
     }
 }
